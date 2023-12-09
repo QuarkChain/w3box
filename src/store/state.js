bcc8bf5933db94a7fe9ea31bf075e6519a06164b
iteyelmp@gmail.com <lmp19890127>
2023-12-09 16:40:50 +0800

    change spe network

--- src/store/state.js
+++ src/store/state.js
@@ -3,6 +3,6 @@ export const chains = [{
   FileBoxController:'0xCD469d20bc4da5F97DC350FDB02D6370559C9511',
 },
 {
-  chainID: '0xd06',
-  FileBoxController:'0x731Cd5311e9bAd1E99e7F1081A91a38c02b5F47d',
+  chainID: '0xaa36a7',
+  FileBoxController:'0x33D3708A6F4Ad146B6Bb4Ba10fe797e483e0611c',
 }];
